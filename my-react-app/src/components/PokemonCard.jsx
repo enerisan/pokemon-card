f087ed75dce4596230936be79c00c4e32c3f4229
Irene Sánchez <enerisan@gmail.com>
2024-03-26 16:13:03 +0100

    adding props

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,26 +1,16 @@
-function PokemonCard() {
-    const pokemon = pokemonList[0]
-        
-      
-    return (
-      <figure>
-        {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt ={pokemon.name}></img> : <p>???</p>} 
-       <figcaption>{pokemon.name}</figcaption>
-      </figure>
-    );
+function PokemonCard({pokemon}) {
+
+  return (
+    <figure className="card">
+      {pokemon.imgSrc ? <img className = "card-img" src={pokemon.imgSrc} alt ={pokemon.name}/> : <p>???</p>} 
+     <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
   }
   
  
 
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+
 
   export default PokemonCard;
+
